1673eca9e6a1affd2bfa83ba3408967b1d8a0eef
josephpupo <josephedwardpupo@gmail.com>
2023-04-17 11:49:10 -0400

    routine commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,45 @@
-<script>
-import PlayerInfo from "/Users/josephpupo/Desktop/CSC190/Valorant Store/src/views/player-info.svelte";
-    const player_info = [
-        {
-            name: "crosshair",
-            image_url: "https://www.talkesport.com/wp-content/uploads/image-438.png",
-            description: "dawgies crosshair"
-        },
-        {
-            name: "crosshair 2",
-            image_url: "https://www.talkesport.com/wp-content/uploads/image-439.png",
-            description: "this is another crosshair"
-        }
-    ]
-</script>
-
-
-<header>
-    <h1>Valorant Info</h1>
-</header>
+<div class="header" id= "h1">
+	<h1>valorant informatics</h1>
+</div>
 
 <info-cards>
-    {#each player_info as info}
-        <!-- svelte-ignore component-name-lowercase -->
-        <PlayerInfo info ={info}></PlayerInfo>
-    {/each}
+	{#each player_info as info}
+	<!-- svelte-ignore component-name-lowercase -->
+	<PlayerInfo info ={info}></PlayerInfo>
+	{/each}
 </info-cards>
 
 
-
-
-
 <style>
-    h1{
-        text-align: center;
-    }
-
-    header{
-        grid-area: header;
-        text-align: right;
-    }
-
-    info-cards{
-        display: grid;
-    }
+	.header{
+		padding: 10px;
+		background-color: rgb(0, 0, 0);
+		color:rgb(255, 255, 255);
+		text-align: center;
+		position: sticky;
+		top: 0;
+	}
+	
+	info-cards{
+		display: grid;
+	}
 </style>
+
+
+<script>
+	import PlayerInfo from "/Users/josephpupo/Desktop/CSC190/Valorant Store/valinformatics/src/views/player-info.svelte";
+	const player_info = [
+	{
+		name: "crosshair",
+		image_url: "https://www.talkesport.com/wp-content/uploads/image-438.png",
+		description: "dawgies crosshair"
+	},
+	{
+		name: "crosshair 2",
+		image_url: "https://www.talkesport.com/wp-content/uploads/image-439.png",
+		description: "this is another crosshair"
+	}
+	]
+	
+</script>	 	
+
