2a288f839fa3bf8581d76b27a67ebf05bb31f488
josephpupo <josephedwardpupo@gmail.com>
2023-05-10 18:14:18 -0400

    fully functional image generation (must refresh to see updated png), crashes when null input, copy button not functional

--- src/views/player-info.svelte
+++ src/views/player-info.svelte
@@ -22,37 +22,31 @@ infoCard{
     box-shadow: 1px 1px 10px 0px #b2b2b2;
     border-radius: 10px;
     justify-content: center;
-    grid-auto-rows: max-content;
-    grid-template-columns: 1fr 300px;
-    max-width: 100%;
-
+    max-height: 400px;
         display: grid;  
+
 
         grid-template: 
             "name"
             "image"
             "description"
-            /min-content;
+            /min-content
 }
 
 infoCard img{
-    grid-area: image;
-    justify-content: center;
-    max-height: 25%     
+    grid-area: image; 
+    width: 174px;    
     
 
 }
 
 infoCard [class = 'name']{
     grid-area: name;
-    max-height: 100%;
     font-size: 20px;
-    margin: 0;
 }
 
 infoCard [class = 'description']{
     grid-area: description;
-    max-height: 100%;
     font-size: 50px;
 }
 
